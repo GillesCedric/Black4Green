d912303d8b65abce529aa74f3ad86e58050941bb
Cedrik Nguefack <cedric.nguefack@tapmeppe.com>
2022-10-22 08:15:39 +0200

    Creation du Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { NextComponentType } from 'next'
+import GoodPractices from './index/GoodPractices'
+
+
+const Footer: NextComponentType = () => {
+	return <footer className='bg-gray-900'>
+		
+	</footer>
+}
+
+export default Footer
