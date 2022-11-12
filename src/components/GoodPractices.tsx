cd4673d03227e2d8e12a2c5e8377d6337634ac8c
Cedrik Nguefack <cedric.nguefack@tapmeppe.com>
2022-11-12 18:56:35 +0100

    Création des composant GoodPractices et GoodPractice

---
+++ src/components/GoodPractices.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { GoodPractices as GoodPracticesType } from '../models/GoodPractices'
+import Accordion from './Accordion'
+
+interface PropsGoodPractices {
+  goodPractices: GoodPracticesType[]
+  recommendations: string[]
+}
+
+interface StateGoodPractices {
+  goodPractices: GoodPracticesType[]
+}
+
+export default class GoodPractices extends React.Component<PropsGoodPractices, StateGoodPractices> {
+  constructor(props: PropsGoodPractices) {
+    super(props)
+    this.state = {
+      goodPractices: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.setState({ goodPractices: this.props.goodPractices })
+  }
+
+  render: () => React.ReactNode = () => {
+    return <div className='bg-gray-100 w-full pb-12'>
+      <Accordion goodPractices={this.state.goodPractices} recommendations={this.props.recommendations} />
+    </div>
+
+  }
+}
