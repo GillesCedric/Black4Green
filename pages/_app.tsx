53d34cec0ec02bd0236fe56e5898e2838f9e54d6
Cedrik Nguefack <cedric.nguefack@tapmeppe.com>
2022-11-12 18:47:06 +0100

    Suppresion du package react-cookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { CookiesProvider } from 'react-cookie'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <CookiesProvider>
-    <Component {...pageProps} />
-  </CookiesProvider>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
